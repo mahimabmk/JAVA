c5331e134d056d3e5fb22a339e91f08186ee9e87
mahima <731mahima@gmail.com>
2024-02-03 22:50:58 +0530

    3rd february

---
+++ oddEvenCheck.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class oddEvenCheck {
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+        if(num%2==0){
+            System.out.println("The number is even.");
+        }
+        else{
+            System.out.println("The number is odd.");
+        }
+
+    }
+}
