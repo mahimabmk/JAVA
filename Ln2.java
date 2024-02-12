b232210ff94c12fc99a89f3dbe8d1d7c073ae3a0
mahima <731mahima@gmail.com>
2024-02-12 13:34:08 +0530

    12th february

---
+++ Ln2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Ln2 {
+    public static void main(String[] args) {
+        int num;
+        double sum =0;
+
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        num = console.nextInt();
+
+        for(int i =1; i<=num; i++){
+            if(i%2==0){
+                sum-=1.0/i;
+            }
+            else{
+                sum+=1.0/i;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
