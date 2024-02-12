81f6587763d0728538fe847fa3b969a2e9944e1a
mahima <731mahima@gmail.com>
2024-02-12 14:18:24 +0530

    pattern

---
+++ rightHalfPyramid.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class rightHalfPyramid {
+    public static void main(String[] args) {
+        int num;
+
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        num = console.nextInt();
+
+        for(int i=1; i<=num ;i++){
+            for( int j=1; j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
