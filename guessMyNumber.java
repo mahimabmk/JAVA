ef21ec91fcaf1d131d2ec9f82429cb3958d6c593
mahima <731mahima@gmail.com>
2024-02-12 14:13:09 +0530

    12th february

---
+++ guessMyNumber.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class guessMyNumber {
+    public static void main(String[] args) {
+        int num;
+        int guess;
+        int tries=0;
+        Scanner console = new Scanner(System.in);
+
+        num = (int) (Math.random() * 100) + 1;
+
+        System.out.println("Guess my number");
+        
+
+        do{
+            System.out.println("Enter a number: ");
+            guess = console.nextInt();
+
+            tries++;
+
+            if(guess>num){
+                System.out.println("BIgger number try lower.");
+            }
+            else if(guess<num){
+                System.out.println("lower number, try higher.");
+            }
+            else{
+                System.out.println("correct "+ tries+ " these are the number of tries.");
+            }
+        }while (guess!=num);
+
+
+    }
+}
