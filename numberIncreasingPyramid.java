437ef9f92367ed4a970ea4de3a17f069957c785a
mahima <731mahima@gmail.com>
2024-02-13 18:33:36 +0530

    patterns

---
+++ numberIncreasingPyramid.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class numberIncreasingPyramid {
+    public static void main(String[] args) {
+        int num;
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        num= console.nextInt();
+
+        for(int i=0; i<=num; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+    }
+}
