69ba8e4dc1eaa2c0850e3c7675403ff808cfeaba
mahima <731mahima@gmail.com>
2024-02-13 18:41:43 +0530

    pattern

---
+++ reverseRightPyramid.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class reverseRightPyramid {
+    public static void main(String[] args) {
+        int num;
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        num= console.nextInt();
+
+        for(int i=num; i>=1 ; i--){
+            for(int j=1; j<=i ;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
