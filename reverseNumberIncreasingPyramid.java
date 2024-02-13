bc8c9d67d0370e8ebb1c69288257e5e8c660aa0f
mahima <731mahima@gmail.com>
2024-02-13 18:44:42 +0530

    pattern

---
+++ reverseNumberIncreasingPyramid.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class reverseNumberIncreasingPyramid {
+    public static void main(String[] args) {
+        int num;
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        num= console.nextInt();
+
+        for(int i=num; i>=1 ; i--){
+            for(int j=1; j<=i ;j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+    }
+}
